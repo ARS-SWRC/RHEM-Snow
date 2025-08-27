6cfcbab4de9d6f4a70cf1b53489bc62d5457ab33
Patrick Broxton <broxtopd@users.noreply.github.com>
2025-08-27 12:11:42 -0700

    Add files via upload

---
+++ Python313/demo_coupledmodel.bat
@@ -0,0 +1,3 @@
+REM k2_snow_v2.exe <Kineros PAR File> <KINEROS Output Directory> <KINEROS CN> <CLIGEN stm file> <RHEM-Snow Output Directory> <Soil Type> <Slope> <Aspect>
+k2_snow_v2.exe data_input_id_17159_485055_201156019020701R2.PAR ./Output 2 wy485055.stm ./Output Loam 5.14 270.
+pause
