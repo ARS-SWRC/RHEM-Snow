44f7be6a447251db09b05a730619abed953120ab
Patrick Broxton <broxtopd@users.noreply.github.com>
2025-08-27 12:08:16 -0700

    Update demo_standalonemodel.bat

--- Python311/demo_standalonemodel.bat
+++ Python311/demo_standalonemodel.bat
@@ -1,3 +1,4 @@
 REM python snow.py <CLIGEN stm file> <RHEM-Snow Output Directory> <Soil Type> <Slope> <Aspect>
-python snow.py ./Input/wy485055.stm ./Output Loam 5.14 270.
+python snow.py wy485055.stm ./Output Loam 5.14 270.
 pause
+
